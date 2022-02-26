b65b179d5bdb737416dff9c59adc726e45c4b796
MADEYE <52187012+seyi91@users.noreply.github.com>
2022-02-25 20:22:43 -0600

    Added logic for addKeyAndValue()

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -43,7 +43,8 @@ function showFirstAndLast(arr){
 }
 
 /*
-Write a function called addKeyAndValue which accepts an array of objects, a key, and a value and returns the array passed to the function with the new key and value added for each object 
+Write a function called addKeyAndValue which accepts an array of objects, a key, and a value and returns the array passed 
+to the function with the new key and value added for each object 
 
 Examples:
     addKeyAndValue([{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}], 'title', 'instructor') 
@@ -52,8 +53,11 @@ Examples:
 
 */
 function addKeyAndValue(arr,key,value){
-    
-}
+    arr.forEach(function(obj){
+        obj[key] = value;
+    });
+    return arr;
+};
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
