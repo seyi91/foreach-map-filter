4472c3cc2c3b71a77cbd8b8215d781e2f6ee4982
MADEYE <52187012+seyi91@users.noreply.github.com>
2022-03-01 11:50:03 -0600

    Added find()

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -186,14 +186,19 @@ function filterByValue(arr, key) {
  }
 
 /*
-Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
+Write a function called find which accepts an array and a value and returns the first element in the
+array that has the same value as the second parameter or undefined if the value is not found in the array.
 
 Examples:
     find([1,2,3,4,5], 3) // 3
     find([1,2,3,4,5], 10) // undefined
 */
 
-function find(arr, searchValue) { }
+function find(arr, searchValue) {
+    return arr.filter(function(val){
+        return val === searchValue;
+    })[0];
+ }
 
 /*
 Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
