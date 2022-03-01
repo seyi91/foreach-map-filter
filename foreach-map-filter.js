fe7d00b1f7545551f9be8c9afbacfae48bfc81b0
MADEYE <52187012+seyi91@users.noreply.github.com>
2022-03-01 11:39:40 -0600

    Add extractFullName()

--- foreach-map-filter.js
+++ foreach-map-filter.js
@@ -157,14 +157,18 @@ function extractKey(arr, key) {
 }
 
 /*
-Write a function called extractFullName which accepts an array of objects and returns a new array with the value of the key with a name of "first" and the value of a key with the name of  "last" in each object, concatenated together with a space. 
+Write a function called extractFullName which accepts an array of objects and returns a new array with the value of the 
+key with a name of "first" and the value of a key with the name of  "last" in each object, concatenated together 
+with a space. 
 
 Examples:
     extractFullName([{first: 'Elie', last:"Schoppik"}, {first: 'Tim', last:"Garcia"}, {first: 'Matt', last:"Lane"}, {first: 'Colt', last:"Steele"}]) // ['Elie Schoppik', 'Tim Garcia', 'Matt Lane', 'Colt Steele']
 */
 
 function extractFullName(arr) {
-
+    return arr.map(function(val){
+        return `${val.first} ${val.last}`;
+    })
 }
 
 /*
